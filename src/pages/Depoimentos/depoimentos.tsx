45edba144e55cdf07fddff22ae548b4b1c0f2b8f
Jonathan <jonathan.reis.torres@gmail.com>
2022-09-11 15:28:51 -0300

    'commit-silvio-roberto-revisado'

--- src/pages/Depoimentos/depoimentos.tsx
+++ src/pages/Depoimentos/depoimentos.tsx
@@ -1,85 +1,127 @@
-import React from 'react';
-import AdocaoImg from "../../assets/images/adocao.jpg"
-import FredImg from "../../assets/images/fred.jpg"
-import MelImg from "../../assets/images/mel.jpeg"
-import TobiasImg from "../../assets/images/tobias.jpg"
-import JulieImg from "../../assets/images/julie.jpg"
-import TomImg from "../../assets/images/tom.jpg"
-import TomasImg from "../../assets/images/tomas.jpg"
+import React from "react";
+import AdocaoImg from "../../assets/images/Adocao.jpg";
+import FredImg from "../../assets/images/fred.jpg";
+import MelImg from "../../assets/images/mel.jpeg";
+import TobiasImg from "../../assets/images/tobias.jpg";
+import JulieImg from "../../assets/images/julie.jpg";
+import TomImg from "../../assets/images/tom.jpg";
+import TomasImg from "../../assets/images/tomas.jpg";
 
 import {
-	Container,
-    Body,
-    Main,
-    TituloBanner,
-    DepoimentosBanner,
-    DepoimentoApresentacao,
-    TituloDescricao,
-    TextoDepoimento,
-    PrincipalDepoimento,
-    DepoimentoUl,
-    DepoimentoLi,
-    H3,
-    TextoApDepoimento,
-    Texto,
-    FotosDepoimento
+  Container,
+  Body,
+  Main,
+  TituloBanner,
+  DepoimentosBanner,
+  DepoimentoApresentacao,
+  TituloDescricao,
+  TextoDepoimento,
+  PrincipalDepoimento,
+  DepoimentoUl,
+  DepoimentoLi,
+  H3,
+  TextoApDepoimento,
+  Texto,
+  FotosDepoimento,
 } from "./styles";
 
 const Depoimentos = () => {
   return (
-<Body>
-    <Main>
-			<section>
-				<TituloBanner>Nossos&nbsp;Depoimentos</TituloBanner>
-				<DepoimentosBanner src={AdocaoImg} alt="Foto de um garoto com um filhote de cachorro"/>
-			</section>
+    <Body>
+      <Main>
+        <section>
+          <TituloBanner>Nossos&nbsp;Depoimentos</TituloBanner>
+          <DepoimentosBanner
+            src={AdocaoImg}
+            alt="Foto de um garoto com um filhote de cachorro"
+          />
+        </section>
 
-			<DepoimentoApresentacao>
-				<TituloDescricao>Veja os depoimentos dos nossos AUMIGOS</TituloDescricao>
-			</DepoimentoApresentacao>
+        <DepoimentoApresentacao>
+          <TituloDescricao>
+            Veja os depoimentos dos nossos AUMIGOS
+          </TituloDescricao>
+        </DepoimentoApresentacao>
 
-			<PrincipalDepoimento>
-				<DepoimentoUl>
-					<DepoimentoLi>
-						<H3>Fred</H3>
-						<FotosDepoimento src={FredImg} alt="Foto de Uma garota com um Gato Filhote"/>
-						<TextoApDepoimento>Eu não adotei o Fred, foi ele quem me adotou! Logo que fui conhece-lo foi amor a primeira vista, obrigada AUMIGOS.</TextoApDepoimento>
-					</DepoimentoLi>
+        <PrincipalDepoimento>
+          <DepoimentoUl>
+            <DepoimentoLi>
+              <H3>Fred</H3>
+              <FotosDepoimento
+                src={FredImg}
+                alt="Foto de Uma garota com um Gato Filhote"
+              />
+              <TextoApDepoimento>
+                Eu não adotei o Fred, foi ele quem me adotou! Logo que fui
+                conhece-lo foi amor a primeira vista, obrigada AUMIGOS.
+              </TextoApDepoimento>
+            </DepoimentoLi>
 
-					<DepoimentoLi>
-						<H3>Mel</H3>
-						<FotosDepoimento src={MelImg} alt="Foto de uma Garota com uma cachorrinha"/> 
-						<TextoApDepoimento>Adotei a Mel tem 1 mês e não conseguimos mais ficar longe uma da outra. Obrigada AUMIGOS por todo o suporte durante o processo!</TextoApDepoimento>
-					</DepoimentoLi>
+            <DepoimentoLi>
+              <H3>Mel</H3>
+              <FotosDepoimento
+                src={MelImg}
+                alt="Foto de uma Garota com uma cachorrinha"
+              />
+              <TextoApDepoimento>
+                Adotei a Mel tem 1 mês e não conseguimos mais ficar longe uma da
+                outra. Obrigada AUMIGOS por todo o suporte durante o processo!
+              </TextoApDepoimento>
+            </DepoimentoLi>
 
-					<DepoimentoLi>
-						<H3>Tobias</H3>
-						<FotosDepoimento src={TobiasImg} alt="Foto de um senhor com um cachorro idoso"/> 
-						<TextoApDepoimento>Adotei um animalzinho mais velho , eles são incríveis e merecem um lar também! Esse sou eu e o Tobias que ja tem 5 Anos, minha paixão!</TextoApDepoimento>
-					</DepoimentoLi>
+            <DepoimentoLi>
+              <H3>Tobias</H3>
+              <FotosDepoimento
+                src={TobiasImg}
+                alt="Foto de um senhor com um cachorro idoso"
+              />
+              <TextoApDepoimento>
+                Adotei um animalzinho mais velho , eles são incríveis e merecem
+                um lar também! Esse sou eu e o Tobias que ja tem 5 Anos, minha
+                paixão!
+              </TextoApDepoimento>
+            </DepoimentoLi>
 
-					<DepoimentoLi>
-						<H3>Julie</H3>
-						<FotosDepoimento src={JulieImg} alt="Foto de uma garotinha com seu cão branco e marrom"/> 
-						<TextoApDepoimento>Essa é minha irmã e a Julie, acho que foram feitas uma para a outra, obrigada AUMIGOS por tudo!</TextoApDepoimento>
-					</DepoimentoLi>
+            <DepoimentoLi>
+              <H3>Julie</H3>
+              <FotosDepoimento
+                src={JulieImg}
+                alt="Foto de uma garotinha com seu cão branco e marrom"
+              />
+              <TextoApDepoimento>
+                Essa é minha irmã e a Julie, acho que foram feitas uma para a
+                outra, obrigada AUMIGOS por tudo!
+              </TextoApDepoimento>
+            </DepoimentoLi>
 
-					<DepoimentoLi>
-						<H3>Tom</H3>
-						<FotosDepoimento src={TomImg} alt="Foto de uma idosa com seu cão filhote"/>
-						<TextoApDepoimento>Adotei o Tom, que ainda era um filhote, para a minha avó, não preciso nem dizer o quanto ela é apaixonada por ele né !?</TextoApDepoimento>
-					</DepoimentoLi>
+            <DepoimentoLi>
+              <H3>Tom</H3>
+              <FotosDepoimento
+                src={TomImg}
+                alt="Foto de uma idosa com seu cão filhote"
+              />
+              <TextoApDepoimento>
+                Adotei o Tom, que ainda era um filhote, para a minha avó, não
+                preciso nem dizer o quanto ela é apaixonada por ele né !?
+              </TextoApDepoimento>
+            </DepoimentoLi>
 
-					<DepoimentoLi>
-						<H3>Tomas</H3>
-						<FotosDepoimento src={TomasImg} alt="Foto de uma garotinha com seu gato"/>
-						<TextoApDepoimento>Obrigada AUMIGOS agradeço pelo suporte durante todo o processo de adoção, agora a Bianca tem o Tomas para brincar!</TextoApDepoimento>
-					</DepoimentoLi>
-				</DepoimentoUl>
-			</PrincipalDepoimento>
-		</Main>
-</Body>
+            <DepoimentoLi>
+              <H3>Tomas</H3>
+              <FotosDepoimento
+                src={TomasImg}
+                alt="Foto de uma garotinha com seu gato"
+              />
+              <TextoApDepoimento>
+                Obrigada AUMIGOS agradeço pelo suporte durante todo o processo
+                de adoção, agora a Bianca tem o Tomas para brincar!
+              </TextoApDepoimento>
+            </DepoimentoLi>
+          </DepoimentoUl>
+        </PrincipalDepoimento>
+      </Main>
+    </Body>
   );
 };
 
-export default Depoimentos; 
+export default Depoimentos;
--- src/pages/Depoimentos/styles.ts
+++ src/pages/Depoimentos/styles.ts
@@ -2,147 +2,138 @@ import styled from "styled-components";
 
 const Container = styled.input``;
 
-const Body = styled.body`
-    background-color: #DDDDDD;
-    font-family: "Montserrat", sans-serif;
-`;
+const Body = styled.body``;
 
 const Main = styled.main`
-    background-color: #DDDDDD;
+  padding: 20px;
+  border-radius: 15px;
 `;
-  
+
 const TituloBanner = styled.h2`
-    font-family: "Italianno", cursive;
-    position: absolute;
-    display: inline-block;
-    margin-left: 40px;
-    margin-top: 80px;
-    font-weight: bold;
-    color: #a04d20;
-    text-align: center;
-    font-size: 4.125rem;
-`;  
+  position: absolute;
+  display: inline-block;
+  margin-left: 40px;
+  margin-top: 80px;
+  font-weight: bold;
+  color: #a04d20;
+  text-align: center;
+  font-size: 4.125rem;
+`;
 
 const DepoimentosBanner = styled.img`
-    margin-top: 30px;
-    display: inline-block;
-    width: 100%;
-    height: 630px;
-    border-radius: 17px;
+  margin-top: 30px;
+  display: inline-block;
+  width: 100%;
+  height: 630px;
+  border-radius: 17px;
 `;
 
 const DepoimentoApresentacao = styled.section`
-    background: linear-gradient(#DDDDDD, #888888);
-    margin-top: 40px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    padding-left: 35px;
-    padding-right: 25px;
+  margin-top: 40px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 35px;
+  padding-right: 25px;
 `;
 
 const TituloDescricao = styled.h2`
-    margin-top: -10px;
-    width: 500px;
-    font-family: "New Rocker", cursive;
-    margin-bottom: 30px;
-    margin-left: 350px;
-    margin-right: 500px;
-    padding: 20px;
-    text-align: center;
-    border: 2px solid rgba(124, 10, 39, 0.925);
-    border-radius: 10px;
-    box-shadow: 10px 10px 10px #373737;
+  margin-top: -10px;
+  width: 500px;
+  margin-bottom: 30px;
+  margin-left: 350px;
+  margin-right: 500px;
+  padding: 20px;
+  text-align: center;
+  border: 2px solid rgba(124, 10, 39, 0.925);
+  border-radius: 10px;
+  box-shadow: 10px 10px 10px #373737;
 `;
-  
+
 const TextoApDepoimento = styled.p`
-    text-align: center;
-    margin-left: 33px;
-    margin-right: 33px;
-    font-size: 1.65rem;
-    font-family: 'Cormorant Garamond', serif;
+  text-align: center;
+  margin-left: 33px;
+  margin-right: 33px;
+  font-size: 1.15rem;
 `;
-  
+
 const PrincipalDepoimento = styled.section`
-    background: linear-gradient(#DDDDDD, #888888);
+  background: linear-gradient(#dddddd, #888888);
+  border-radius: 10px;
 `;
-  
+
 const DepoimentoUl = styled.ul`
-    display: inline-block;
+  display: inline-block;
 `;
-  
-  
+
 const DepoimentoLi = styled.li`
-    display: inline-block;
-    vertical-align: top;
-    width: 20%;
-    margin: 25px;
-    margin-top: 100px;
-    margin-left: 7%;
-    margin-right: 4%;
-    border: solid 2px rgba(124, 10, 39, 0.925);
-    border-radius: 15px;
-    background-color: #CCCCCC;
-    box-shadow: 10px 10px 10px #000000;
-    font-size: 1.25rem;
- }
- &:hover {
+  display: inline-block;
+  vertical-align: top;
+  width: 20%;
+  margin: 25px;
+  margin-top: 100px;
+  margin-left: 7%;
+  margin-right: 4%;
+  border: solid 2px rgba(124, 10, 39, 0.925);
+  border-radius: 15px;
+  background-color: #cccccc;
+  box-shadow: 10px 10px 10px #000000;
+  font-size: 1.25rem;
+  &:hover {
     transform: scale(1.1);
-    border-color: #C78C19;
+    border-color: #c78c19;
     font-size: 2rem;
-}
- &:active {
-    border-color: #088C19;
- }  
+  }
+  &:active {
+    border-color: #088c19;
+  }
 `;
-  
+
 const H3 = styled.h3`
-    font-family: "New Rocker", cursive;
-    margin-top: 35px;
-    text-align: center;
-    font-size: 1.375rem;
+  margin-top: 35px;
+  text-align: center;
+  font-size: 1.375rem;
 `;
-  
+
 const TextoDepoimento = styled.p`
-    font-family: "Italianno", cursive;
-    text-align: center;
-    margin-top: 50px;
-    font-size: 1.75rem;
+  text-align: center;
+  margin-top: 50px;
+  font-size: 1.75rem;
 `;
 
 const Texto = styled.p`
-    margin-bottom: 0px 20px 10px 10px
+  margin-bottom: 0px 20px 10px 10px;
 `;
-  
+
 const FotosDepoimento = styled.img`
-    margin-top: 30px;
-    width: 286px;
-    height: 400px;
-    margin-left: 4px;
-    margin-right: 4px;
-    border-radius: 35px;
-    filter: grayscale(100%);
-    transition: 3s all;
- }
- &:hover {
+  display: flex;
+  width: 200px;
+  padding: 18px;
+  height: 300px;
+  margin-left: 4px;
+  margin-right: 4px;
+  border-radius: 35px;
+  filter: grayscale(100%);
+  transition: 3s all;
+  &:hover {
     transform: scale(1.1);
     filter: grayscale(0%);
- }`
-  
+  }
+`;
 
 export {
-    Container,
-    Body,
-    Main,
-    TituloBanner,
-    DepoimentosBanner,
-    DepoimentoApresentacao,
-    TituloDescricao,
-    TextoDepoimento,
-    PrincipalDepoimento,
-    DepoimentoUl,
-    DepoimentoLi,
-    H3,
-    TextoApDepoimento,
-    Texto,
-    FotosDepoimento
+  Container,
+  Body,
+  Main,
+  TituloBanner,
+  DepoimentosBanner,
+  DepoimentoApresentacao,
+  TituloDescricao,
+  TextoDepoimento,
+  PrincipalDepoimento,
+  DepoimentoUl,
+  DepoimentoLi,
+  H3,
+  TextoApDepoimento,
+  Texto,
+  FotosDepoimento,
 };
